c4f2a4d14f42328c86af19afd0a02218e2b4f914
Faridg79 <farid.79.dev@gmail.com>
2023-09-09 10:28:45 +0330

    Add class for get images path

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
-"""You Project Goes Here."""
+"""Import the necessary modules to build the game."""
+from os import path
 
-print("Hello Karyar")
+
+class GetImage:
+    """This class gives us the image path by image name."""
+
+    __main_path = path.join("assets", "images") + path.sep
+
+    def __init__(self, image_name: str):
+        """Initialize class GetImage."""
+        assert isinstance(image_name, str), "addr is not str"
+        self.__image_name = image_name
+
+    @property
+    def main_path(self):
+        """main_path return the main path."""
+        return GetImage.__main_path
+
+    @property
+    def get_image_path(self):
+        """get_image_path return the image path."""
+        return GetImage.__main_path + self.__image_name
